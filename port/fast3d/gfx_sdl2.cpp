e995f90f7900dd1e05478d528b63345087d61297
sandstranger <sandstranger@gmail.com>
2026-05-18 10:44:15 +0400

    disable SDL_WINDOW_RESIZABLE on Android

--- port/fast3d/gfx_sdl2.cpp
+++ port/fast3d/gfx_sdl2.cpp
@@ -163,7 +163,7 @@ static void gfx_sdl_init(const struct GfxWindowInitSettings *set) {
     }
 #else
     fullscreen_state = true;
-    Uint32 flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP;
+    Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP;
 #endif
 
 #ifdef SDL_WINDOW_ALLOW_HIGHDPI
